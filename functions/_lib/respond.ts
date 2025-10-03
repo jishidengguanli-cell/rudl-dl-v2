c3dd3ad9d468bf3406c24645592b570e765d0b32
jishidengguanli-cell <jishidengguanli@gmail.com>
2025-10-03 15:20:58 +0800

    0320

--- functions/_lib/respond.ts
+++
@@ -1,5 +0,0 @@
-export const text = (body: string, status = 200, headers: Record<string, string> = {}) =>
-  new Response(body, { status, headers: { "content-type": "text/plain; charset=utf-8", ...headers } });
-
-export const json = (data: unknown, status = 200, headers: Record<string, string> = {}) =>
-  new Response(JSON.stringify(data), { status, headers: { "content-type": "application/json; charset=utf-8", ...headers } });
